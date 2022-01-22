cc679a663f11206d93b5a80095863a72d3ca52ac
Black_block <46107558+johnson1205@users.noreply.github.com>
2022-01-22 20:01:09 +0800

    Add files via upload
    I spend lots of time on entering my student id and password. I hate it and it also waste my time.

---
+++ LOG_IN_NTUST_MOODLE.py
@@ -0,0 +1,12 @@
+from threading import activeCount
+from selenium import webdriver
+import bs4
+browser = webdriver.Chrome()
+url = 'https://moodle.ntust.edu.tw/'
+browser.get(url)
+accout = browser.find_element_by_xpath("//input[@id='login_username']")
+accout.send_keys('STUDENT ID')
+password = browser.find_element_by_xpath("//input[@id='login_password']")
+password.send_keys('PAWWSORD')
+button = browser.find_element_by_xpath('/html[1]/body[1]/div[3]/section[2]/div[1]/section[1]/aside[1]/div[2]/div[2]/form[1]/div[4]/input[1]')
+button.click()
